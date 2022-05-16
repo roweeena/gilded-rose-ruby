e33ac45b6583cca7bbc261f1b1e386d5a506678e
roweeena <roe0320@gmail.com>
2022-05-16 11:14:40 +1000

    refactoring part of the class

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,52 +4,53 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name == "Sulfuras, Hand of Ragnaros"
-        next # guard clause
-      end
+  def update_item(item)
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-
-            item.quality = item.quality - 1
-
+            item.quality -= 1
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          item.quality += 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality +=  1
               end
             end
           end
         end
       end
-        item.sell_in = item.sell_in - 1
-
+        item.sell_in -=  1
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              item.quality = item.quality - 1
-
+              item.quality -=  1
             end
           else
             item.quality = item.quality - item.quality
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            item.quality += 1
           end
         end
       end
+
+  end
+
+  def update_quality()
+    @items.each do |item|
+      if item.name == "Sulfuras, Hand of Ragnaros"
+        next # guard clause
+      end
+       update_item(item)
     end
   end
 end
