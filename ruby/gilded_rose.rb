1226b3140a2e516e476ac82e3f96560a1f57a7ce
roweeena <roe0320@gmail.com>
2022-05-17 10:48:20 +1000

    separating items by their conditions

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -5,52 +5,51 @@ class GildedRose
   end
 
   def update_item(item)
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
+    if item.name == "Aged Brie"
+      if item.quality < 50
+          item.quality += 1
+      end
+      item.sell_in -=  1
+      return
+    end
+
+    if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      if item.quality < 50
+        if item.sell_in > 10
+          item.quality += 1
+        elsif item.sell_in <= 5
+          item.quality += 3
+        elsif item.sell_in <= 10
+          item.quality += 2
+
+        end
+
+      end
+      item.sell_in -=  1
+      if item.sell_in < 0
+        item.quality = item.quality - item.quality
+      end
+      return
+    end
+
+#common items without special conditions
+        if item.quality > 0 && item.sell_in > 0
             item.quality -= 1
         end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality +=  1
-              end
-            end
-          end
-        end
-      end
+
         item.sell_in -=  1
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              item.quality -=  1
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality += 1
-          end
+      if item.sell_in < 0 && item.quality > 0
+              item.quality -=  2
         end
-      end
 
   end
 
   def update_quality()
     @items.each do |item|
       if item.name == "Sulfuras, Hand of Ragnaros"
-        next # guard clause
+        next # guard clause (only for enum)
       end
-       update_item(item)
+      update_item(item)
     end
   end
 end
